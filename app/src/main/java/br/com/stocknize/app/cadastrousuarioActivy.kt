1c515d7cf2450efc878be6697b03f19de39f3892
felipenanes <felipenanes@gmail.com>
2021-05-27 23:47:08 -0300

    Ajustando os arquivos de layout no novo projeto e criando os constraints deles para que em landscape o frontend não quebre

--- app/src/main/java/br/com/stocknize/app/cadastrousuarioActivy.kt
+++ app/src/main/java/br/com/stocknize/app/cadastrousuarioActivy.kt
@@ -6,9 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class CadastroUsuario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastrousuario)
-
-
-
+        setContentView(R.layout.activity_cad_usuario)
     }
 }
